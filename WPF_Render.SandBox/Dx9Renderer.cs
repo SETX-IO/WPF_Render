51e5d4ca48727a1c183377b4948f79f4b7027f3e
SETSVPC <Hatsune_Miku_3939@outlook.com>
2025-09-27 21:23:19 +0800

    渲染三角形

---
+++ WPF_Render.SandBox/Dx9Renderer.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace WPF_Render.SandBox;
+
+public class Dx9Renderer
+{
+    [DllImport("WPF_Render.Renderder.dll")]
+    public static extern int GetBackBufferNoRef(out IntPtr pSurface);
+
+    [DllImport("WPF_Render.Renderder")]
+    public static extern int SetSize(uint width, uint height);
+
+    [DllImport("WPF_Render.Renderder.dll")]
+    public static extern int SetAlpha(bool useAlpha);
+
+    [DllImport("WPF_Render.Renderder.dll")]
+    public static extern int SetNumDesiredSamples(uint numSamples);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public POINT(Point p)
+        {
+            x = (int)p.X;
+            y = (int)p.Y;
+        }
+
+        public int x;
+        public int y;
+    }
+
+    [DllImport("WPF_Render.Renderder.dll")]
+    public static extern int SetAdapter(POINT screenSpacePoint);
+
+    [DllImport("WPF_Render.Renderder.dll")]
+    public static extern int Render();
+
+    [DllImport("WPF_Render.Renderder.dll")]
+    public static extern void Destroy();
+}
+
+public static class HRESULT
+{
+    public static void Check(int hr)
+    {
+        Marshal.ThrowExceptionForHR(hr);
+    }
+}
