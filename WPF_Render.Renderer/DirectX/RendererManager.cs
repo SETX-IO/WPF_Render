b31a91e4f0ccd0b60cb7844cdaaae5bd4a4f6056
SETSVPC <Hatsune_Miku_3939@outlook.com>
2025-09-29 01:23:53 +0800

    尝试全面转向C#,结果失败。

---
+++ WPF_Render.Renderer/DirectX/RendererManager.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using Vortice.Direct3D9;
+
+namespace WPF_Render.Renderer.DirectX;
+
+public class RendererManager
+{
+    private IDirect3D9? _d3d9;
+    private IDirect3D9Ex? _d3d9Ex;
+
+    private uint _adapters;
+    private Renderer[] _rgRenderers;
+    private Renderer _currentRenderer;
+
+    private IntPtr _hwnd;
+    
+    private uint _width;
+    private uint _height;
+    private uint _numSamples;
+    private bool _isUseAlpha;
+    private bool _surfaceSettingsChanged;
+
+    private RendererManager() { }
+    
+    public void SetSize(uint width, uint height)
+    {
+        if (width == _width && height == _height) return;
+        
+        _width = width;
+        _height = height;
+        _surfaceSettingsChanged = true;
+    }
+
+    public void SetAlpha(bool useAlpha)
+    {
+        if (_isUseAlpha == useAlpha) return;
+        
+        _isUseAlpha = useAlpha;
+        _surfaceSettingsChanged = true;
+    }
+
+    public void SetNumDesiredSamples(uint numSamples)
+    {
+        if (_numSamples == numSamples) return;
+
+        _numSamples = numSamples;
+        _surfaceSettingsChanged = true;
+    }
+
+    public void GetBackBufferNoRef(out IDirect3DSurface9? pSurface)
+    {
+        CleanupInvalidDevices();
+        
+        EnsureD3DObjects();
+
+        EnsureRenderers();
+        
+        if (_surfaceSettingsChanged)
+        {
+            for (uint i = 0; i < _adapters; i++)
+            {
+                _rgRenderers[i].CreateSurface(_width, _height, _isUseAlpha, _numSamples);
+            }
+
+            _surfaceSettingsChanged = false;
+        }
+
+        pSurface = _currentRenderer.Surface;
+    }
+
+    [DllImport("User32.dll")]
+    private static extern IntPtr MonitorFromPoint([In]Point pt, [In]uint dwFlags);
+    
+    public void SetAdapter(Point screenSpacePoint)
+    {
+        CleanupInvalidDevices();
+        
+        if (_d3d9 is null || _rgRenderers.Length is not 0) return;
+        
+        var hMon = MonitorFromPoint(screenSpacePoint, 0);
+        for (uint i = 0; i < _adapters; i++)
+        {
+            if (hMon == _d3d9?.GetAdapterMonitor(i))
+            {
+                _currentRenderer = _rgRenderers[i];
+                break;
+            }
+        }
+    }
+    
+    public void Render() => _currentRenderer.Render();
+    
+    private void EnsureD3DObjects()
+    {
+        _d3d9Ex ??= D3D9.Direct3DCreate9Ex();
+        _d3d9 ??= _d3d9Ex.QueryInterface<IDirect3D9>();
+
+        _adapters = _d3d9.AdapterCount;
+    }
+    
+    private void EnsureRenderers()
+    {
+        Debug.Assert(_adapters is not 0);
+        _rgRenderers = new Renderer[_adapters];
+
+        for (uint i = 0; i < _adapters; i++)
+        {
+            TriangleRenderer.Create(_d3d9, _d3d9Ex, _hwnd, i, out var renderer);
+            _rgRenderers[i] = renderer;
+        }
+
+        _currentRenderer = _rgRenderers[0];
+    }
+
+    private void CleanupInvalidDevices()
+    {
+        for (uint i = 0; i < _adapters; i++)
+        {
+            if (!_rgRenderers[i].CheckDeviceState()) continue;
+            
+            DestroyResources();
+            break;
+        }
+    }
+
+    private void DestroyResources()
+    {
+        _d3d9?.Release();
+        _d3d9Ex?.Release();
+
+        _adapters = 0;
+        _surfaceSettingsChanged = true;
+    }
+
+    public static RendererManager Create(Window window)
+    {
+        var rendererManager = new RendererManager
+        {
+            _hwnd = ((HwndSource?)PresentationSource.FromVisual(window)).Handle,
+        };
+
+        return rendererManager;
+    }
+}
--- WPF_Render.SandBox/MainWindow.xaml.cs
+++ WPF_Render.SandBox/MainWindow.xaml.cs
@@ -2,162 +2,169 @@
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Threading;
+using Vortice.Direct3D9;
+using WPF_Render.Renderer.DirectX;
 
-namespace WPF_Render.SandBox
+namespace WPF_Render.SandBox;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, IDisposable
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    DispatcherTimer _sizeTimer;
+    DispatcherTimer _adapterTimer;
+    TimeSpan _lastRender;
+
+    private RendererManager _rendererManager;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    void SizeTimer_Tick(object sender, EventArgs e)
+    {
+        // The following code does not account for RenderTransforms.
+        // To handle that case, you must transform up to the root and
+        // check the size there.
+
+        // Given that the D3DImage is at 96.0 DPI, its Width and Height
+        // properties will always be integers. ActualWidth/Height
+        // may not be integers, so they are cast to integers.
+        uint actualWidth = (uint)imgelt.ActualWidth;
+        uint actualHeight = (uint)imgelt.ActualHeight;
+        if ((actualWidth > 0 && actualHeight > 0) &&
+            (actualWidth != (uint)d3dimg.Width || actualHeight != (uint)d3dimg.Height))
         {
-            InitializeComponent();
-
-            // Set up the initial state for the D3DImage.
-            HRESULT.Check(Dx9Renderer.SetSize((uint)Width, (uint)Height));
-            HRESULT.Check(Dx9Renderer.SetAlpha(true));
-            HRESULT.Check(Dx9Renderer.SetNumDesiredSamples(4));
-
-            //
-            // Optional: Subscribing to the IsFrontBufferAvailableChanged event.
-            //
-            // If you don't render every frame (e.g. you only render in
-            // reaction to a button click), you should subscribe to the
-            // IsFrontBufferAvailableChanged event to be notified when rendered content
-            // is no longer being displayed. This event also notifies you when
-            // the D3DImage is capable of being displayed again.
-
-            // For example, in the button click case, if you don't render again when
-            // the IsFrontBufferAvailable property is set to true, your
-            // D3DImage won't display anything until the next button click.
-            //
-            // Because this application renders every frame, there is no need to
-            // handle the IsFrontBufferAvailableChanged event.
-            //
-            CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
-
-            //
-            // Optional: Multi-adapter optimization
-            //
-            // The surface is created initially on a particular adapter.
-            // If the WPF window is dragged to another adapter, WPF
-            // ensures that the D3DImage still shows up on the new
-            // adapter.
-            //
-            // This process is slow on Windows XP.
-            //
-            // Performance is better on Vista with a 9Ex device. It's only
-            // slow when the D3DImage crosses a video-card boundary.
-            //
-            // To work around this issue, you can move your surface when
-            // the D3DImage is displayed on another adapter. To
-            // determine when that is the case, transform a point on the
-            // D3DImage into screen space and find out which adapter
-            // contains that screen space point.
-            //
-            // When your D3DImage straddles two adapters, nothing
-            // can be done, because one will be updating slowly.
-            //
-            _adapterTimer = new DispatcherTimer();
-            _adapterTimer.Tick += new EventHandler(AdapterTimer_Tick);
-            _adapterTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
-            _adapterTimer.Start();
-
-            //
-            // Optional: Surface resizing
-            //
-            // The D3DImage is scaled when WPF renders it at a size
-            // different from the natural size of the surface. If the
-            // D3DImage is scaled up significantly, image quality
-            // degrades.
-            //
-            // To avoid this, you can either create a very large
-            // texture initially, or you can create new surfaces as
-            // the size changes. Below is a very simple example of
-            // how to do the latter.
-            //
-            // By creating a timer at Render priority, you are guaranteed
-            // that new surfaces are created while the element
-            // is still being arranged. A 200 ms interval gives
-            // a good balance between image quality and performance.
-            // You must be careful not to create new surfaces too
-            // frequently. Frequently allocating a new surface may
-            // fragment or exhaust video memory. This issue is more
-            // significant on XDDM than it is on WDDM, because WDDM
-            // can page out video memory.
-            //
-            // Another approach is deriving from the Image class,
-            // participating in layout by overriding the ArrangeOverride method, and
-            // updating size in the overriden method. Performance will degrade
-            // if you resize too frequently.
-            //
-            // Blurry D3DImages can still occur due to subpixel
-            // alignments.
-            //
-            _sizeTimer = new DispatcherTimer(DispatcherPriority.Render);
-            _sizeTimer.Tick += new EventHandler(SizeTimer_Tick);
-            _sizeTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
-            _sizeTimer.Start();
+            _rendererManager.SetSize(actualWidth,actualHeight);
         }
+    }
 
-        ~MainWindow()
+    void CompositionTarget_Rendering(object sender, EventArgs e)
+    {
+        RenderingEventArgs args = (RenderingEventArgs)e;
+
+        // It's possible for Rendering to call back twice in the same frame
+        // so only render when we haven't already rendered in this frame.
+        if (d3dimg.IsFrontBufferAvailable && _lastRender != args.RenderingTime)
         {
-            Dx9Renderer.Destroy();
-        }
-
-        void AdapterTimer_Tick(object sender, EventArgs e)
-        {
-            Dx9Renderer.POINT p = new Dx9Renderer.POINT(imgelt.PointToScreen(new Point(0, 0)));
-
-            HRESULT.Check(Dx9Renderer.SetAdapter(p));
-        }
-
-        void SizeTimer_Tick(object sender, EventArgs e)
-        {
-            // The following code does not account for RenderTransforms.
-            // To handle that case, you must transform up to the root and
-            // check the size there.
-
-            // Given that the D3DImage is at 96.0 DPI, its Width and Height
-            // properties will always be integers. ActualWidth/Height
-            // may not be integers, so they are cast to integers.
-            uint actualWidth = (uint)imgelt.ActualWidth;
-            uint actualHeight = (uint)imgelt.ActualHeight;
-            if ((actualWidth > 0 && actualHeight > 0) &&
-                (actualWidth != (uint)d3dimg.Width || actualHeight != (uint)d3dimg.Height))
+            // IntPtr pSurface = IntPtr.Zero;
+            // HRESULT.Check(Dx9Renderer.GetBackBufferNoRef(out pSurface));
+            _rendererManager.GetBackBufferNoRef(out var surface);
+            
+            if (surface is not null)
             {
-                HRESULT.Check(Dx9Renderer.SetSize(actualWidth, actualHeight));
+                d3dimg.Lock();
+                // Repeatedly calling SetBackBuffer with the same IntPtr is
+                // a no-op. There is no performance penalty.
+                d3dimg.SetBackBuffer(D3DResourceType.IDirect3DSurface9, surface.NativePointer);
+                // HRESULT.Check(Dx9Renderer.Render());
+                _rendererManager.Render();
+                d3dimg.AddDirtyRect(new Int32Rect(0, 0, d3dimg.PixelWidth, d3dimg.PixelHeight));
+                d3dimg.Unlock();
+
+                _lastRender = args.RenderingTime;
             }
         }
+    }
 
-        void CompositionTarget_Rendering(object sender, EventArgs e)
-        {
-            RenderingEventArgs args = (RenderingEventArgs)e;
+    public void Dispose()
+    {
+        Dx9Renderer.Destroy();
+    }
 
-            // It's possible for Rendering to call back twice in the same frame
-            // so only render when we haven't already rendered in this frame.
-            if (d3dimg.IsFrontBufferAvailable && _lastRender != args.RenderingTime)
-            {
-                IntPtr pSurface = IntPtr.Zero;
-                HRESULT.Check(Dx9Renderer.GetBackBufferNoRef(out pSurface));
-                if (pSurface != IntPtr.Zero)
-                {
-                    d3dimg.Lock();
-                    // Repeatedly calling SetBackBuffer with the same IntPtr is
-                    // a no-op. There is no performance penalty.
-                    d3dimg.SetBackBuffer(D3DResourceType.IDirect3DSurface9, pSurface);
-                    HRESULT.Check(Dx9Renderer.Render());
-                    d3dimg.AddDirtyRect(new Int32Rect(0, 0, d3dimg.PixelWidth, d3dimg.PixelHeight));
-                    d3dimg.Unlock();
+    private void MainWindow_OnSourceInitialized(object? sender, EventArgs e)
+    {
+        _rendererManager = RendererManager.Create(this);
 
-                    _lastRender = args.RenderingTime;
-                }
-            }
-        }
+        // Set up the initial state for the D3DImage.
+        _rendererManager.SetSize((uint)Width, (uint)Height);
+        _rendererManager.SetAlpha(true);
+        _rendererManager.SetNumDesiredSamples(4);
+        // HRESULT.Check(Dx9Renderer.SetSize((uint)Width, (uint)Height));
+        // HRESULT.Check(Dx9Renderer.SetAlpha(true));
+        // HRESULT.Check(Dx9Renderer.SetNumDesiredSamples(4));
 
-        DispatcherTimer _sizeTimer;
-        DispatcherTimer _adapterTimer;
-        TimeSpan _lastRender;
+        //
+        // Optional: Subscribing to the IsFrontBufferAvailableChanged event.
+        //
+        // If you don't render every frame (e.g. you only render in
+        // reaction to a button click), you should subscribe to the
+        // IsFrontBufferAvailableChanged event to be notified when rendered content
+        // is no longer being displayed. This event also notifies you when
+        // the D3DImage is capable of being displayed again.
+
+        // For example, in the button click case, if you don't render again when
+        // the IsFrontBufferAvailable property is set to true, your
+        // D3DImage won't display anything until the next button click.
+        //
+        // Because this application renders every frame, there is no need to
+        // handle the IsFrontBufferAvailableChanged event.
+        //
+        CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+        //
+        // Optional: Multi-adapter optimization
+        //
+        // The surface is created initially on a particular adapter.
+        // If the WPF window is dragged to another adapter, WPF
+        // ensures that the D3DImage still shows up on the new
+        // adapter.
+        //
+        // This process is slow on Windows XP.
+        //
+        // Performance is better on Vista with a 9Ex device. It's only
+        // slow when the D3DImage crosses a video-card boundary.
+        //
+        // To work around this issue, you can move your surface when
+        // the D3DImage is displayed on another adapter. To
+        // determine when that is the case, transform a point on the
+        // D3DImage into screen space and find out which adapter
+        // contains that screen space point.
+        //
+        // When your D3DImage straddles two adapters, nothing
+        // can be done, because one will be updating slowly.
+        //
+        _adapterTimer = new DispatcherTimer();
+        _adapterTimer.Tick += (_, _) => _rendererManager.SetAdapter(imgelt.PointFromScreen(new Point(0, 0)));
+        _adapterTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+        _adapterTimer.Start();
+
+        //
+        // Optional: Surface resizing
+        //
+        // The D3DImage is scaled when WPF renders it at a size
+        // different from the natural size of the surface. If the
+        // D3DImage is scaled up significantly, image quality
+        // degrades.
+        //
+        // To avoid this, you can either create a very large
+        // texture initially, or you can create new surfaces as
+        // the size changes. Below is a very simple example of
+        // how to do the latter.
+        //
+        // By creating a timer at Render priority, you are guaranteed
+        // that new surfaces are created while the element
+        // is still being arranged. A 200 ms interval gives
+        // a good balance between image quality and performance.
+        // You must be careful not to create new surfaces too
+        // frequently. Frequently allocating a new surface may
+        // fragment or exhaust video memory. This issue is more
+        // significant on XDDM than it is on WDDM, because WDDM
+        // can page out video memory.
+        //
+        // Another approach is deriving from the Image class,
+        // participating in layout by overriding the ArrangeOverride method, and
+        // updating size in the overriden method. Performance will degrade
+        // if you resize too frequently.
+        //
+        // Blurry D3DImages can still occur due to subpixel
+        // alignments.
+        //
+        _sizeTimer = new DispatcherTimer(DispatcherPriority.Render);
+        _sizeTimer.Tick += SizeTimer_Tick;
+        _sizeTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+        _sizeTimer.Start();
     }
 }
